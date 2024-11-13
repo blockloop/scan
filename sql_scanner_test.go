69291e172473cff5ebb2092bd110e5c9d009320e
sashayakovtseva <sashayakovtseva@gmail.com>
2024-11-13 21:15:33 +0300

    Use ramsql

--- sql_scanner_test.go
+++ sql_scanner_test.go
@@ -1,54 +1,39 @@
-package scan
+package scan_test
 
 import (
-	"context"
-	"database/sql"
 	"fmt"
-	"log"
+	"strconv"
 	"testing"
 
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/blockloop/scan/v2"
 )
 
 func TestCustomScanner(t *testing.T) {
 	t.Parallel()
 
-	dsn, cleanup, err := testDatabaseDSN()
-	require.NoError(t, err)
-	t.Cleanup(cleanup)
-
-	db, err := sql.Open("mysql", dsn)
-	require.NoError(t, err)
+	db := mustDB("sql_scanner", `
+		CREATE TABLE test
+		(
+			id int PRIMARY KEY,
+			data int NOT NULL
+		)`,
+		`INSERT INTO test (id, data) VALUES (1, 123), (2, 234)`,
+	)
 	t.Cleanup(func() { db.Close() })
-
-	const createTableQuery = `
-CREATE TABLE test
-(
-	id int PRIMARY KEY AUTO_INCREMENT,
-	data int NOT NULL
-);
-
-INSERT INTO test (id, data)
-VALUES (1, 123), (2, 234);
-`
 
 	const (
 		selectOneQuery = `SELECT data FROM test WHERE id = 1`
 		selectAllQuery = `SELECT data FROM test`
 	)
 
-	_, err = db.Exec(createTableQuery)
-	require.NoError(t, err)
-
 	t.Run("scan.Row must work", func(t *testing.T) {
 		var data customScanner
 		rows, err := db.Query(selectOneQuery)
 		require.NoError(t, err)
 
-		err = Row(&data, rows)
+		err = scan.Row(&data, rows)
 		require.NoError(t, err)
 		require.Equal(t, customScanner{v: 123}, data)
 	})
@@ -58,7 +43,7 @@ VALUES (1, 123), (2, 234);
 		rows, err := db.Query(selectAllQuery)
 		require.NoError(t, err)
 
-		err = Rows(&data, rows)
+		err = scan.Rows(&data, rows)
 		require.NoError(t, err)
 		require.ElementsMatch(t, []customScanner{{v: 123}, {v: 234}}, data)
 	})
@@ -70,10 +55,15 @@ type customScanner struct {
 }
 
 func (c *customScanner) Scan(src any) error {
-	log.Println("custom scan")
 	switch v := src.(type) {
 	case int64:
 		*c = customScanner{v: v}
+	case []byte: // for ramsql
+		n, err := strconv.ParseInt(string(v), 10, 64)
+		if err != nil {
+			return fmt.Errorf("parse int: %w", err)
+		}
+		*c = customScanner{v: n}
 	case nil:
 		return nil
 	default:
@@ -81,45 +71,4 @@ func (c *customScanner) Scan(src any) error {
 	}
 
 	return nil
-}
-
-func testDatabaseDSN() (string, func(), error) {
-	mysqlContainer, err := testcontainers.GenericContainer(context.Background(),
-		testcontainers.GenericContainerRequest{
-			ContainerRequest: testcontainers.ContainerRequest{
-				Image:        "percona/percona-server:8.0.36-28.1-multi",
-				ExposedPorts: []string{"3306/tcp"},
-				WaitingFor: wait.ForAll(
-					wait.ForListeningPort("3306/tcp"),
-					wait.ForLog("mysqld: ready for connections"),
-				),
-				Env: map[string]string{
-					"MYSQL_DATABASE":             "test",
-					"MYSQL_ALLOW_EMPTY_PASSWORD": "yes",
-				},
-			},
-			Started: true,
-		})
-	if err != nil {
-		return "", nil, err
-	}
-
-	cleanup := func() {
-		_ = mysqlContainer.Terminate(context.Background())
-	}
-
-	host, err := mysqlContainer.Host(context.Background())
-	if err != nil {
-		cleanup()
-		return "", nil, err
-	}
-
-	port, err := mysqlContainer.MappedPort(context.Background(), "3306")
-	if err != nil {
-		cleanup()
-		return "", nil, err
-	}
-
-	dsn := fmt.Sprintf("root@(%s:%d)/test?tls=false&multiStatements=true", host, port.Int())
-	return dsn, cleanup, nil
 }
